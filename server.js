31455b5b9bf9e7a078d0ce6384badde0eaf1dff5
hugoalle <hugoallemand41@hotmail.com>
2022-10-13 23:01:20 +0200

    ajout une recette dans database

--- server.js
+++ server.js
@@ -6,6 +6,36 @@ mongoose.connect('mongodb://localhost/cuisine-kit')
     .then(() => console.log('connected to cuisine-kit database'))
     .catch((err) => console.error('fail to connect to database', err))
 
+const recetteSchema = new mongoose.Schema({
+    titre: String, // String is shorthand for {type: String}
+    nbPart: Number,
+    duree: String,
+    ingredients: [{ ingredient: String, quantite: Number, unite: String }],
+    etapes: [String]
+})
+
+const RecetteClasse = mongoose.model('Recette', recetteSchema)
+
+async function createRecette() {
+
+    const recette = new RecetteClasse({
+        titre: 'Crepe',
+        nbPart: 4,
+        duree: '50min',
+        ingredients: [
+            { ingredient: 'Farine', quantite: 200, unite: 'gramme' },
+            { ingredient: 'Lait', quantite: 500, unite: 'mL' },
+            { ingredient: 'Oeuf', quantite: 4, unite: '' }],
+        etapes: ['Mettre farine', 'mettre le reste', 'faire cuir']
+    })
+
+    const result = await recette.save()
+    console.log(result)
+}
+
+createRecette();
+
+
 const app = express()
 app.use(express.static(__dirname + '/static'));
 
