153d1a915a524c274f7b842d95dffc70c106d1e4
ALLEMAND Hugo <hugo.allemand@soprasteria.com>
2022-10-16 18:25:57 +0200

    ajout champ etape recette

--- static/views/NewRecipeView.js
+++ static/views/NewRecipeView.js
@@ -2,9 +2,10 @@ export default class NewRecipeView {
     constructor(container) {
         this.container = container
         this.listIngredientInput = []
-        this.recipeStepsInput = []
+        this.listRecipeStepsInput = []
 
         this.ingredientsPart = this.addIngredientsPart()
+        this.recipeStepsPart = this.addRecipePart()
 
     }
 
@@ -25,6 +26,23 @@ export default class NewRecipeView {
         return containerElement
     }
 
+    addRecipePart() {
+        const containerElement = document.createElement('div')
+        containerElement.innerHTML = "<p> Etapes de la recette</p>"
+
+        const firstEtapeInput = this.addRecipeStepInput(this.listRecipeStepsInput.length + 1)
+        containerElement.appendChild(firstEtapeInput)
+
+        const buttonAddNewRecipeStep = document.createElement('button')
+        // buttonAddNewRecipeStep.classList.add('buttonAddAnotherIngredient')
+        buttonAddNewRecipeStep.textContent = 'Ajouter une etape'
+        buttonAddNewRecipeStep.addEventListener('click', () => {
+            containerElement.insertBefore(this.addRecipeStepInput(this.listRecipeStepsInput.length + 1), buttonAddNewRecipeStep)
+        })
+        containerElement.appendChild(buttonAddNewRecipeStep)
+        return containerElement
+    }
+
     addIngredientInput(i) {
         const divContainer = document.createElement('div')
         divContainer.innerHTML = `<p> Ingrédient ${i}`
@@ -37,7 +55,20 @@ export default class NewRecipeView {
         return divContainer
     }
 
+    addRecipeStepInput(i) {
+        const divContainer = document.createElement('div')
+        divContainer.innerHTML = `<p> Etape ${i}`
+
+        const input = document.createElement('input')
+        input.classList.add('inputRecipeStep')
+        this.listRecipeStepsInput.push(input)
+
+        divContainer.appendChild(input)
+        return divContainer
+    }
+
     display() {
         this.container.appendChild(this.ingredientsPart)
+        this.container.appendChild(this.recipeStepsPart)
     }
 }
